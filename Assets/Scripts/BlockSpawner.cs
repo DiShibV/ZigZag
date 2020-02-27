e4d8ca17624e0064eb2a09a491d6db5d886cfb59
DiMoNx174x <dragonx244x@yandex.ru>
2020-02-27 08:46:29 +0500

    manipulation with the movements of objects
    - Add snap Camera
    - Add  move player

---
+++ Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+    [SerializeField] private int _countMaxBlock;
+    [SerializeField] private GameObject _prefabBlock;
+    private readonly Vector3[] _startPointsOfCreateBlock = new Vector3[]{new Vector3(0,0,2),new Vector3(-1,0,2),new Vector3(-2,0,1)};
+
+
+
+}
---
+++ Assets/Scripts/CameraSnap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSnap : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    private Transform _centerForCamera;
+
+    private void Start()
+    {
+        _centerForCamera = transform;
+    }
+
+    private void Update()
+    {
+        AcceptPositionCamera();
+    }
+
+    private void AcceptPositionCamera(){
+        _centerForCamera.position = Vector3.forward * _target.position.z;
+    }
+
+}
